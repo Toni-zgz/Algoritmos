88b70d0492f703a423ac27cfd9ddf5a2a897342d
toni_zgz <>
2022-04-12 08:05:21 +0000

    Pila ahora es un tipo de datos inmutable

--- Tema 5/Pila.clj
+++ Tema 5/Pila.clj
@@ -4,11 +4,11 @@
   (stk-pop [this])
   (stk-peek [this]))
 
-(deftype Pila [^:unsynchronized-mutable content]
+(deftype Pila [content]
   Apilable
   (stk-push [this, elt]
     (if (list? content)
-      (set! content (conj content elt))
+      (Pila. (conj content elt))
       (println "Error: No es un objeto Pila")))
   (stk-peek [this]
     (if (list? content)
@@ -16,19 +16,19 @@
       (println "Error: No es un objeto Pila")))
   (stk-pop [this]
     (if (list? content)
-      (set! content (pop content))
+      (Pila. (pop content))
       (println "Error: No es un objeto Pila"))))
 
 ;;; Ejemplo de utilización
 (require '[clojure.test :as test])
 (def a (->Pila '()))
+(def b (->Pila '(3)))
+(def c (->Pila '(4 3)))
 (test/testing "pila-tests"
          (test/is (= (stk-peek a) nil))
-         (test/is (= (stk-push a 3) '(3)))
-         (test/is (= (stk-peek a) 3))
-         (test/is (= (stk-push a 4) '(4 3)))
-         (test/is (= (stk-peek a) 4))
-         (test/is (= (stk-pop a) '(3)))
-         (test/is (= (stk-peek a) 3))
-         (test/is (= (stk-pop a) '()))
-         (test/is (= (stk-peek a) nil)))
+         (test/is (= (stk-peek b) 3))
+         (test/is (= (stk-peek c) 4))
+         (test/is (= (stk-peek (stk-push a 3)) 3))
+         (test/is (= (stk-peek (stk-push b 4)) 4))
+         (test/is (= (stk-peek (stk-pop c)) 3))
+         (test/is (= (stk-peek (stk-pop b)) nil)))
