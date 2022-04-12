b5373ae24650d84ffb6b2a955ed1a1bf00b3bb51
toni_zgz <>
2022-04-12 21:07:35 +0000

    Se mejora la documentación del tipo Cola.

--- Tema 5/Cola.clj
+++ Tema 5/Cola.clj
@@ -1,4 +1,5 @@
 (ns Cola)
+; Protocolos
 (defprotocol IEncolable
   (que-queue [this, elt])
   (que-dequeue [this])
@@ -7,23 +8,40 @@
 (defprotocol IEquiv
   (equiv [this other]))
 
+; Tipo Cola
 (deftype Cola [content]
   IEncolable
+  ; que-queue :: Cola -> a -> Cola
+  ; Este método introduce un valor en el frente
+  ; de la cola, devolviendo la cola modificada.
   (que-queue [this, elt]
     (if (vector? content)
       (let [list-nueva (concat [elt] content)
             vector-nuevo (into [] list-nueva)]
        (Cola. vector-nuevo))
       (println "Error: No es un objeto Cola")))
+  ; que-peek :: Cola -> a
+  ; Este método devuelve el valor que esta en 
+  ; la parte trasera de la Cola, sin modificar
+  ; la cola.
   (que-peek [this]
     (if (vector? content)
       (peek content)
       (println "Error: No es un objeto Cola")))
+  ; que-dequeue :: Cola -> [a, Cola]
+  ; Este método devuelve el valor que esta en 
+  ; la parte trasera de la Cola, y la cola 
+  ; sin ese valor.
   (que-dequeue [this]
     (if (vector? content)
       (vector (peek content)(Cola. (pop content)))
       (println "Error: No es un objeto Cola")))
+  
   IEquiv
+  ; equiv :: Cola -> Cola -> Boolean
+  ; Este método devuelve true si los argumentos
+  ; tienen el mismo tipo y el mismo valor de 
+  ; content.
   (equiv [this other] 
          (and 
           (= (type this) (type other))
