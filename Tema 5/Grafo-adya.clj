a70716d731fe4b49eca27820a4aada914f39ba5f
toni_zgz <>
2022-04-22 20:47:10 +0000

    modificamos nuevo-grafo-adya para contemplar un vector de aristas y un vector de nodos

--- Tema 5/Grafo-adya.clj
+++ Tema 5/Grafo-adya.clj
@@ -6,7 +6,18 @@
         (= (.valor this) (.valor other))
         (= (.adyacente this) (.adyacente other)))))
 
-(defn nuevo-grafo-adya [num-elems]
-  (let [vec-valor (make-array Integer/TYPE num-elems)
-        ary-adyac (make-array Boolean/TYPE num-elems num-elems)]
-    (->Grafo-adya vec-valor ary-adyac)))
+(defn nuevo-grafo-adya [nodos aristas]
+  (let [num-elems (count nodos)
+        rango (range 1 (+ num-elems 1))
+        ary-adyac (make-array Boolean/TYPE num-elems num-elems)
+        indices (range (count aristas))
+        mapeo-nodos (zipmap nodos indices)
+        lista-indices (map (fn [elt] (map (fn [elt2] (get mapeo-nodos elt2)) elt)) aristas)]
+    (dorun (map (fn [elt] (aset ary-adyac (first elt) (second elt) true)) lista-indices))
+    (->Grafo-adya nodos ary-adyac)))
+
+;;; Ejemplo de utilización
+(require '[clojure.test :as test])
+(def nodos [:alfa, :beta, :gamma, :delta])
+(def aristas ['(:alfa :beta) '(:alfa :gamma) '(:beta :delta) '(:gamma :alfa) '(:gamma :beta) '(:gamma :delta)])
+(def grafo-a (nuevo-grafo-adya nodos aristas))
