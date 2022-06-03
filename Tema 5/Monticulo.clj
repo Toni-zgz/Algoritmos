8964fbd368f21aa57bc3fad956e26b075da55afb
toni_zgz <ajaczgz@yahoo.es>
2022-06-03 18:37:01 +0000

    Añado la función añadir-nodo

--- Tema 5/Monticulo.clj
+++ Tema 5/Monticulo.clj
@@ -24,7 +24,7 @@
 (defn obtener-raiz [mont]
   (get (.datos mont) 0))
 
-; hundir :: Monticulo -> Integer -> Monticulo
+; hundir :: Monticulo -> Integer -> [Integer]
 ; esta función hunde el nodo i para restablecer la propiedad del 
 ; monticulo.
 (defn hundir [mont nodo]
@@ -34,8 +34,8 @@
     (loop [datos-bucle datos
            k (- nodo 1)]
         (let [j k
-              Tk (get datos-bucle k)
               T2j (get datos-bucle (* 2 j))
+              Tk (get datos-bucle k)
               T2j+1 (get datos-bucle (+ (* 2 j) 1))
               Tj (get datos-bucle j)
               nuevo-k1 (if (and (<= (* 2 j) n)
@@ -52,9 +52,35 @@
                              (assoc j nuevo-Tk)
                              (assoc nuevo-k2 Tj))]
           (if (= j nuevo-k2)
-              datos-nuevos
+              datos-nuevos 
               (recur datos-nuevos nuevo-k2))))))
-  
+
+; flotar :: Monticulo -> Integer -> [Integer]
+; esta función hace flotar el nodo i para restablecer la propiedad del 
+; monticulo.
+(defn flotar [mont nodo]
+  (let [datos (.datos mont)
+        n (- (count datos) 1)
+        op (.comparador mont)]
+    (loop [datos-bucle datos
+           k nodo]
+        (let [j k
+              j2 (quot j 2)
+              Tk (get datos-bucle (- k 1))
+              Tj2 (get datos-bucle (- j2 1))
+              Tj (get datos-bucle (- j 1))
+              nuevo-k (if (and (> j 1)
+                               (not (op Tj2 Tk))) 
+                           j2
+                           k)
+              nuevo-Tk (get datos-bucle (- nuevo-k 1))
+              datos-nuevos (-> datos-bucle 
+                             (assoc (- j 1) nuevo-Tk)
+                             (assoc (- nuevo-k 1) Tj))]
+          (if (= j nuevo-k)
+              datos-nuevos
+              (recur datos-nuevos nuevo-k))))))
+
 ; iniciar :: Monticulo -> Monticulo
 ; esta función inicializa un monticulo.
 (defn iniciar [mont]
@@ -71,6 +97,18 @@
                   nuevo-mont (->Monticulo datos-bucle comparador num-hijos)
                   nuevos-datos-bucle (hundir nuevo-mont indice)]
               (recur (rest indices) nuevos-datos-bucle))))))
+
+; anadir-nodo :: Monticulo -> Integer -> Monticulo
+; Esta función añade un nuevo elemento a un monticulo
+(defn anadir-nodo [mont valor]
+  (let [datos (.datos mont)
+        comparador (.comparador mont)
+        num-hijos (.numero-hijos mont)
+        nuevos-datos (conj datos valor)
+        nuevo-mont (->Monticulo nuevos-datos comparador num-hijos)
+        num-elem (count datos)
+        ary-salida (flotar nuevo-mont (+ num-elem 1))]
+    (->Monticulo ary-salida comparador num-hijos)))
 
 ; crear-monticulo-binario :: Secuencia -> Monticulo
 ; Esta función genera un monticulo binario a partir 
@@ -94,9 +132,11 @@
      (let [m1 (crear-monticulo-binario '(1 6 9 2 7 5 2 7 4 10))
            m2 (->Monticulo '(10 9 7 7 6 5 2 2 4 1) > 2)
            m3 (crear-monticulo-binario-invertido '(1 6 9 2 7 5 2 7 4 10))
-           m4 (->Monticulo '(1 2 4 2 7 5 6 7 9 10) < 2)]
+           m4 (->Monticulo '(1 2 4 2 7 5 6 7 9 10) < 2)
+           m5 (->Monticulo '(11 10 7 7 9 5 2 2 4 1 6) > 2)]
      (test/is (= m1 m2))
      (test/is (= m3 m4))
      (test/is (= (obtener-raiz m1) 10))
      (test/is (= (obtener-raiz m3) 1))
-     (test/is (= (.datos (crear-monticulo-binario '())) []))))
+     (test/is (= (.datos (crear-monticulo-binario '())) []))
+     (test/is (= (anadir-nodo m1 11) m5))))
