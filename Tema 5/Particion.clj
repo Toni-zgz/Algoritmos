ce3c51dbcc58255b5ff3a26dea9e43602d8be17e
toni_zgz <ajaczgz@yahoo.es>
2022-06-13 18:46:42 +0000

    Modifico particion para que sea más funcional y no tan OOP

--- Tema 5/Particion.clj
+++ Tema 5/Particion.clj
@@ -1,43 +1,40 @@
 (ns Particion)
 
-;Protocolo
-(defprotocol IParticion
-  (buscar [this elt])
-  (fusionar [this elt1 elt2]))
-
 ;Tipo Particion
 (deftype Particion [conjunto altura]
-  IParticion
-  ; buscar :: Particion -> a -> a
-  ; Este método busca la raiz del grupo en el que está el argumento
-  ; dentro de la partición.
-  (buscar [this elt]
-    (loop [r elt]
-                 (let [nueva-r (get conjunto (- r 1))]
-                   (if (= r nueva-r) 
-                     r
-                     (recur nueva-r)))))
-
-  ; fusionar :: Particion -> a -> a -> Particion
-  (fusionar [this elt1 elt2]
-        (let [altura-1 (get altura (- elt1 1))
-              altura-2 (get altura (- elt2 1))]
-          (if (= altura-1 altura-2)
-            (let [nueva-altura-1 (+ altura-1 1)
-                  nueva-altura (assoc altura (- elt1 1) nueva-altura-1)
-                  nuevo-conjunto (assoc conjunto (- elt2 1) elt1)]
-              (Particion. nuevo-conjunto nueva-altura))
-            (if (> altura-1 altura-2)
-              (let [nuevo-conjunto (assoc conjunto (- elt2 1) elt1)]
-                (Particion. nuevo-conjunto altura))
-              (let [nuevo-conjunto (assoc conjunto (- elt1 1) elt2)]
-                (Particion. nuevo-conjunto altura))))))
   java.lang.Object
   (equals [this other]
      (and
         (= (type this) (type other))
         (= (.conjunto this) (.conjunto other))
         (= (.altura this) (.altura other)))))
+
+; buscar :: Particion -> a -> a
+; Esta función busca la raiz del grupo en el que está el argumento
+; dentro de la partición.
+(defn buscar [particion elt]
+  (loop [r elt]
+    (let [nueva-r (get (.conjunto particion) (- r 1))]
+      (if (= r nueva-r) 
+        r
+        (recur nueva-r)))))
+
+  ; fusionar :: Particion -> a -> a -> Particion
+(defn fusionar [particion elt1 elt2]
+  (let [altura (.altura particion)
+        conjunto (.conjunto particion)
+        altura-1 (get altura (- elt1 1))
+        altura-2 (get altura (- elt2 1))]
+    (if (= altura-1 altura-2)
+        (let [nueva-altura-1 (+ altura-1 1)
+              nueva-altura (assoc altura (- elt1 1) nueva-altura-1)
+              nuevo-conjunto (assoc conjunto (- elt2 1) elt1)]
+          (->Particion nuevo-conjunto nueva-altura))
+      (if (> altura-1 altura-2)
+        (let [nuevo-conjunto (assoc conjunto (- elt2 1) elt1)]
+          (Particion. nuevo-conjunto altura))
+        (let [nuevo-conjunto (assoc conjunto (- elt1 1) elt2)]
+          (Particion. nuevo-conjunto altura))))))
 
 ; nueva-particion :: Integer -> Particion
 ; Esta función crea una particion inicializada
